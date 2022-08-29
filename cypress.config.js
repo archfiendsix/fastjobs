44a615f75b2e818ade9bec9b6e0d3be0025a9744
Ace Simon <ace.simon.g@gmail.com>
2022-08-29 19:21:11 +0800

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  "watchForFileChanges": true,
+    "reporter": "mochawesome",
+    "reporterOptions": {
+        "charts":true,
+        "overwrite": true,
+        "html":true,
+        "json":true,
+        "reportDir": "cypress/report/mochawesome-report"
+    }
+});
---
+++ cypress/e2e/correct-creds.cy.js
@@ -0,0 +1,28 @@
+describe('Login test cases', () => {
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.visit('https://www.fastjobs.ph/site/login')
+      })
+    it('Login with correct credentials', () => {
+
+        //Normal Login process using valid credentials
+        
+        cy.get(':nth-child(5) > .form-control').type('ace.simon.g@gmail.com')
+        cy.get('#password').type('91366Fs8')
+        cy.get('.form-group > .btn').click()
+    })
+
+    it('Login with incorrect credentials', () => {
+
+        //Normal Login process using valid credentials
+        cy.get(':nth-child(5) > .form-control').type('invalidemail@mail.com')
+        cy.get('#password').type('invalidpassword123')
+        cy.get('.form-group > .btn').click()
+    
+        //Validates alert message of incorrect credentials entered
+        cy.get('form[action="/site/login"] .alert').contains('Invalid email and/or password.').should('exist')
+      })
+})
